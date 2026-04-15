d5778b27f13f9348a858713c4eb8248aa0532722
HANSYTRADER <hansondan12@gmail.com>
2026-04-16 00:53:38 +0300

    Update server.py

--- server.py
+++ server.py
@@ -44,5 +44,8 @@ def signals():
         "signals": results
     })
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
